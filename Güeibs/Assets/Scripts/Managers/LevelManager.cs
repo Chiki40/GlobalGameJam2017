4f649c1aa8c33f9e990a134baa060a653760ef90
victor martinez simon <victormartinezsimon@gmail.com>
2017-01-21 00:43:44 +0100

    add endLevel, Death, LevelManager

---
+++ Güeibs/Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public int LevelID;
+    private static LevelManager m_instance;
+
+    void Awake()
+    {
+        m_instance = this;
+    }
+
+    public static LevelManager GetInstance()
+    {
+        return m_instance;
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void NextLevel()
+    {
+        //TODO call to gamemanager to load the next level
+    }
+}
