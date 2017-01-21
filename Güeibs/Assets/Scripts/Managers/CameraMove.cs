0e55ed0c23d004119a53b34db91e3896bdf36135
morax <m.moracuesta@gmail.com>
2017-01-21 16:24:28 +0100

    Camera in world 1

---
+++ Güeibs/Assets/Scripts/Managers/CameraMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+    public Animator otherAnimator;
+    private Animator m_animatorComponent;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_animatorComponent = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        for (int i = 0; i<otherAnimator.parameterCount;++i)
+        {
+            string name = otherAnimator.parameters[i].name;
+            m_animatorComponent.SetFloat(name,otherAnimator.GetFloat(name));
+        }
+	}
+}
