808478346c64a9e680cdd5ad107080de210b3702
victor martinez simon <victormartinezsimon@gmail.com>
2017-01-21 02:09:01 +0100

    add a wave generator

---
+++ Güeibs/Assets/Scripts/Gameplay/WaveGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveGenerator : MonoBehaviour {
+
+    public GameObject m_wave;
+    public float TimeWave;
+    public float TotalScale;
+    public float TimeBetweenWaves;
+    public Color WaveColor;
+
+    private float timeAcumBetweenWaves;
+    private List<GameObject> listWaves;
+    private List<float> listTimeAcumWave;
+    private static int count = 0;
+    void Start()
+    {
+        timeAcumBetweenWaves = TimeBetweenWaves;// with this hack we have a initial wave
+        listWaves = new List<GameObject>();
+        listTimeAcumWave = new List<float>();
+    }
+
+    void Update()
+    {
+        timeAcumBetweenWaves += Time.deltaTime;
+
+        if (timeAcumBetweenWaves >= TimeBetweenWaves)
+        {
+            GameObject go = Instantiate(m_wave);
+            go.transform.position = this.transform.position;
+            go.transform.localScale = Vector3.zero;
+            Color emptyColor = WaveColor;
+            emptyColor.a = 0;
+            go.GetComponent<Renderer>().material.color = emptyColor;
+            listWaves.Add(go);
+            go.name = count.ToString();
+            ++count;
+            listTimeAcumWave.Add(0);
+            timeAcumBetweenWaves = 0;
+        }
+        List<GameObject> listToDestroy = new List<GameObject>();
+        for (int i = 0; i < listWaves.Count; ++i)
+        {
+            listTimeAcumWave[i] += Time.deltaTime;
+            float timeLerp = listTimeAcumWave[i] / TimeWave;
+            GameObject go = listWaves[i];
+            //Destroy
+            if (listTimeAcumWave[i] >= TimeWave)
+            {
+                listToDestroy.Add(go);
+            }
+            else
+            {
+                //scale
+                go.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one * TotalScale, timeLerp);
+                //alpha
+                Color c = go.GetComponent<Renderer>().material.color;
+                c.a = Mathf.Lerp(1, 0, timeLerp);
+                go.GetComponent<Renderer>().material.color = c;
+            }
+        }
+
+        for (int i = 0; i < listToDestroy.Count; ++i)
+        {
+            int index = listWaves.IndexOf(listToDestroy[i]);
+            listTimeAcumWave.RemoveAt(index);
+            GameObject go = listWaves[i];
+            Destroy(go);
+            listWaves.RemoveAt(index);
+        }
+    }
+}
