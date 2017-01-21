39d311c4306197d5654c8eded7fad82ad019ae14
morax <m.moracuesta@gmail.com>
2017-01-21 19:35:12 +0100

    Level 1 and key.

---
+++ Güeibs/Assets/Scripts/Behaviours/Door.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    Collider m_collider;
+    Animator m_animator;
+
+	// Use this for initialization
+	void Start () {
+        m_collider = GetComponentInChildren<Collider>();
+        m_animator = GetComponentInChildren<Animator>();
+	}
+
+    public void Open()
+    {
+        m_animator.SetTrigger("Open");
+        m_collider.enabled = false;
+    }
+}
