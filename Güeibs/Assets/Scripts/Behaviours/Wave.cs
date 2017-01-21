17a1ab8dfeb55cb714c7c8298c55aeb9467e7319
morax <m.moracuesta@gmail.com>
2017-01-21 20:45:34 +0100

    Wave destroy time

---
+++ Güeibs/Assets/Scripts/Behaviours/Wave.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+    float TimeWave;
+    float TotalScale;
+    Color WaveColor;
+    bool InsideOutside;
+    float TimeAcumWave;
+
+	// Use this for initialization
+	void Start () 
+    {
+        TimeAcumWave = 0;
+        destroySpeed = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        TimeAcumWave += Time.deltaTime;
+        float timeLerp = TimeAcumWave / TimeWave;
+        if (InsideOutside)
+        {
+            transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one * TotalScale, timeLerp);
+        }
+        else
+        {
+            transform.localScale = Vector3.Lerp(Vector3.one * TotalScale, Vector3.zero, timeLerp);
+        }
+        //alpha
+        Color c = GetComponent<Renderer>().material.color;
+        if (destroySpeed == 1)
+        {
+            if (InsideOutside)
+            {
+                c.a = Mathf.Lerp(1, 0, timeLerp);
+            }
+            else
+            {
+                c.a = Mathf.Lerp(0, 1, timeLerp);
+            }
+        }
+        else
+        {
+            c.a = Mathf.Lerp(c.a, 0, destroySpeed);
+        }
+        GetComponent<Renderer>().material.color = c;
+	}
+
+    public void SetValues(float timeWave, float totalScale, Color waveColor, bool insideOutside)
+    {
+        TimeWave = timeWave;
+        TotalScale = totalScale;
+        WaveColor = waveColor;
+        InsideOutside = insideOutside;
+    }
+
+
+    public float destroySpeed { get; set; }
+}
--- Güeibs/Assets/Scripts/Gameplay/WaveGenerator.cs
+++ Güeibs/Assets/Scripts/Gameplay/WaveGenerator.cs
@@ -11,16 +11,13 @@ public class WaveGenerator : MonoBehaviour
     public float TimeBetweenWaves;
     public Color WaveColor;
     public bool InsideToOutside;
+    public float DestroySpeed = 4;
 
     private float timeAcumBetweenWaves;
-    private List<GameObject> listWaves;
-    private List<float> listTimeAcumWave;
     private static int count = 0;
     void Start()
     {
         timeAcumBetweenWaves = TimeBetweenWaves;// with this hack we have a initial wave
-        listWaves = new List<GameObject>();
-        listTimeAcumWave = new List<float>();
     }
 
     void Update()
@@ -36,60 +33,21 @@ public class WaveGenerator : MonoBehaviour
             emptyColor.a = 0;
             go.GetComponent<Renderer>().material.color = emptyColor;
             go.transform.SetParent(this.transform);
-            listWaves.Add(go);
             go.name = count.ToString();
             ++count;
-            listTimeAcumWave.Add(0);
             timeAcumBetweenWaves = 0;
-        }
-        List<GameObject> listToDestroy = new List<GameObject>();
-        for (int i = 0; i < listWaves.Count; ++i)
-        {
-            listTimeAcumWave[i] += Time.deltaTime;
-            float timeLerp = listTimeAcumWave[i] / TimeWave;
-            GameObject go = listWaves[i];
-            //Destroy
-            if (listTimeAcumWave[i] >= TimeWave)
-            {
-                listToDestroy.Add(go);
-            }
-            else
-            {
-                if(go == null)
-                {
-                    listToDestroy.Add(go);
-                    continue;
-                }
-                //scale
-                if (InsideToOutside)
-                {
-                    go.transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one * TotalScale, timeLerp);
-                }
-                else
-                {
-                    go.transform.localScale = Vector3.Lerp(Vector3.one * TotalScale, Vector3.zero, timeLerp);
-                }
-                //alpha
-                Color c = go.GetComponent<Renderer>().material.color;
-                if (InsideToOutside)
-                {
-                    c.a = Mathf.Lerp(1, 0, timeLerp);
-                }
-                else
-                {
-                    c.a = Mathf.Lerp(0, 1, timeLerp);
-                }
-                go.GetComponent<Renderer>().material.color = c;
-            }
-        }
-
-        for (int i = 0; i < listToDestroy.Count; ++i)
-        {
-            int index = listWaves.IndexOf(listToDestroy[i]);
-            listTimeAcumWave.RemoveAt(index);
-            GameObject go = listWaves[i];
-            Destroy(go);
-            listWaves.RemoveAt(index);
+            go.GetComponent<Wave>().SetValues(TimeWave, TotalScale, WaveColor, InsideToOutside);
+            Destroy(go, TimeWave);
         }
     }
+
+    public void OnDisable()
+    {
+        foreach (Transform t in transform)
+        {
+            t.GetComponent<Wave>().destroySpeed = DestroySpeed;
+        }
+    }
+
+
 }
