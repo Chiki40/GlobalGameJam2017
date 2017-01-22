5a91a95db13dcd77ee1ff16318d782be3ce8d3ce
daniel.gonzalez <chiki4089@gmail.com>
2017-01-22 14:07:18 +0100

    - Backbutton on every level except 8 (Victor is modifying it)

---
+++ Güeibs/Assets/Scripts/Gameplay/GameCanvas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCanvas : MonoBehaviour {
+
+	public void GoToMenu() {
+		int rand = Random.Range(1, 4);
+		UtilSound.instance.PlaySound("click" + rand.ToString());
+		GameManager.GetInstance().GoToMenu();
+	}
+}
