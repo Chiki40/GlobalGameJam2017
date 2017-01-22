5e4d11c086c1f640c57a933a6d7386238a0d66a6
daniel.gonzalez <chiki4089@gmail.com>
2017-01-22 13:43:45 +0100

    - Fixed warnings.

--- Güeibs/Assets/Scripts/Behaviours/EndLevelTrigger.cs
+++ Güeibs/Assets/Scripts/Behaviours/EndLevelTrigger.cs
@@ -6,26 +6,16 @@ public class EndLevelTrigger : MonoBehaviour {
 
     PlayerController pc;
     Vector3 initialPlayerPOs;
-    Vector3 distance;
     float acumTime;
     float totalTime = 1;
-    private Coroutine routine;
-    private float speed = 1;
-
-    public void Start()
-    {
-        routine = null;
-    }
 
     void OnTriggerEnter(Collider other)
     {
         pc = other.GetComponent<PlayerController>();
         other.enabled = false;
         pc.enabled = false;
-        distance = transform.position - pc.transform.position;
         initialPlayerPOs = pc.transform.position;
         acumTime = 0;
-        routine = null;
     }
 
     void Update()
@@ -44,7 +34,7 @@ public class EndLevelTrigger : MonoBehaviour {
 
     private IEnumerator DoEndLevel()
     {
-        Debug.Log("we finish the level, play some sound and some animation, particles... whatever");
+        Debug.Log("Level completed");
 		GameManager.GetInstance().NextLevel();
         return null;
     }
--- Güeibs/Assets/Scripts/Gameplay/PlayerController.cs
+++ Güeibs/Assets/Scripts/Gameplay/PlayerController.cs
@@ -100,7 +100,7 @@ public class PlayerController : MonoBehaviour {
 
 	public void OnCollisionStay(Collision col) {
 		if (col.relativeVelocity.magnitude > 20.0f && !UtilSound.instance.IsPlayingFamilySound("crash")) {
-			int rand = Random.Range(1, 4);
+			//int rand = Random.Range(1, 4);
 			//UtilSound.instance.PlaySound("crash" + rand.ToString(), 0.5f);
 		}
 	}
